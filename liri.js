9e642b1f639398d2d6d991968bd4f501f0e123db
xtianares <xtianares@yahoo.com>
2019-02-04 22:45:29 -0500

    working inquirer package with use input

--- liri.js
+++ liri.js
@@ -5,3 +5,24 @@ let keys = require("./keys.js"),
     spotify = new Spotify(keys.spotify)
     inquirer = require('inquirer'), // will use this it ask for user input
     fs = require('fs'); // will use this o log user input history to a file
+
+let userCommand = "",
+    commandInfo = "";
+
+// Prompt the user to provide command.
+inquirer.prompt([
+    {
+        type: "input",
+        name: "userInput",
+        message: "Hi there I'm LIRI, what can I help you with?"
+    }
+])
+.then(function(response) {
+    console.log(response.userInput);
+    let userInput = response.userInput.split(' ');
+    userCommand = userInput.shift();
+    commandInfo = userInput.join(' ');
+
+    console.log(userCommand);
+    console.log(commandInfo);
+});
