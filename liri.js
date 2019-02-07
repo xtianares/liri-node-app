5fa96e41e8420d5f022be1d06c18b0bffbfa70c9
xtianares <xtianares@yahoo.com>
2019-02-06 21:45:46 -0500

    working spotify

--- liri.js
+++ liri.js
@@ -20,6 +20,11 @@ const runCommand = (userCommand, commandInfo) => {
             // console.log(commandInfo);
             concertThis(commandInfo);
             break;
+        case 'spotify-this-song':
+            // console.log(userCommand);
+            // console.log(commandInfo);
+            spotifyThis(commandInfo);
+            break;
         default:
             console.log("I don't know what you mean by that, please check the documentation.");
             break;
@@ -32,18 +37,33 @@ const concertThis = artist => {
         function(response) {
             let events = response.data;
             for (let i = 0; i < events.length; i++) {
+                let {venue, datetime} = events[0];
                 // Name of the venue
-                console.log(events[0].venue.name);
+                console.log('Venue: ' + venue.name);
                 // console.log("The movie's rating is: " + response.data.imdbRating);
                 // Venue location
-                console.log(events[0].venue.city + ', ' + events[0].venue.country);
+                console.log('Location: ' + venue.city + ', ' + venue.country);
                 // Date of the Event (use moment to format this as "MM/DD/YYYY")
-                console.log(moment(events[0].datetime).format("MM/DD/YYYY"));
+                console.log('Date: ' + moment(events[0].datetime).format("MM/DD/YYYY"));
                 // adding line
                 console.log('---------------------');
             }
         }
     );
+}
+const spotifyThis = song => {
+    let query = song != "" ? song : "The Sign";
+    spotify.search({ type: 'track', query: query, limit: 1 })
+        .then(function(response) {
+            let {artists, name, preview_url, album} = response.tracks.items[0];
+            console.log("Artist: " + artists[0].name);
+            console.log("Song Title: " + name);
+            console.log("Preview: " + preview_url);
+            console.log("Album: " + album.name);
+        })
+        .catch(function(err) {
+            console.log(err);
+    });
 }
 
 // Prompt the user to provide command
